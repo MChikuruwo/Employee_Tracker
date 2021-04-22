6bdd4ea07de475523ca5f1503b52f9034b751f86
Munyaradzi Chikuruwo <mchikuruwo@hotmail.com>
2021-04-22 09:38:07 +0200

    initial commit

---
+++ src/main/java/com/employeetracker/EmployeeTrackerAPI/EmployeeTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.employeetracker.EmployeeTrackerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeetracker/EmployeeTrackerAPI/EmployeeTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeetracker.EmployeeTrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
